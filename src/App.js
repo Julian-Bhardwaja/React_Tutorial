5b4e8a5706aea07caea84d7d8d96e9c5c46460c9
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-10-10 22:18:56 +0630

    explaination of props, state, and setState have been added as a note and some code as well!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 
-import Hello from './components/Hello';
-import Welcome from './components/Welcome';
+import Counter from './components/Counter';
+
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-       <Welcome name={'Kronii'} age={20} />
+       {/* <Welcome name={'Kronii'} age={20} />
        <Hello name={'Kronii'} age={20} />
        <Hello name={'Mumei'} age={18} />
        <Hello name={'Bae'} age={19} />
-       <Hello name={'Gura'} age={21} ><p>This is children props</p></Hello>
+       <Hello name={'Gura'} age={21} ><p>This is children props</p></Hello> */}
+       {/* <Message /> */}
+       <Counter />
     </div>
   );
 }
