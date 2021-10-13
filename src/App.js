617fa071461f0bcbc5971e4c1dc01a41b42aa8f7
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-10-13 11:41:13 +0630

    Method as props sending from child components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ClassClick from './components/ClassClick';
 import Counter from './components/Counter';
 import EventBind from './components/EventBind';
 import FunctionClick from './components/FunctionClick';
+import ParentComponent from './components/ParentComponent';
 
 // import Hello from './components/Hello';
 // import Message from './components/Message';
@@ -21,7 +22,8 @@ function App() {
        {/* <Counter /> */}
        {/* <FunctionClick />
        <ClassClick /> */}
-       <EventBind />
+       {/* <EventBind /> */}
+       <ParentComponent />
     </div>
   );
 }
