4ce94305d770f05d7e7e77cd0a8ca63dfb07842e
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-10-13 10:50:23 +0630

    added destructing to the master

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 class Welcome extends Component {
     render() {
-        return <h1>Welcome {this.props.name} and your age is {this.props.age}</h1>
+        const {name, age} = this.props
+        return <h1>Welcome {name} and your age is {age}</h1>
     }
 }
 
