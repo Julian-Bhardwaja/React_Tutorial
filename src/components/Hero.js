fabebd3ca3e0b8701ce19a3a32a86f01fe0a68e0
Julian Bharadwaja <julianbharadwaja1234@gmail.com>
2021-10-19 15:55:50 +0630

    Learnt Render Props

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,10 @@
-import React from 'react'
+import React from "react";
 
 function Hero({ heroName }) {
-    if (heroName === 'Joker') {
-        throw new Error('Not a hero');
-    }
-    return (
-        <div>
-            { heroName }
-        </div>
-    )
+  if (heroName === "Joker") {
+    throw new Error("Not a hero");
+  }
+  return <div>{heroName}</div>;
 }
 
-export default Hero
+export default Hero;
